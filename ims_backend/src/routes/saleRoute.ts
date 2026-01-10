ea7c7699bbd4fe0c80f83ed982e6663198a4fb45
jomi087 <jomijoseph087@gmail.com>
2026-01-10 21:48:34 +0530

    update: type fixed SalesControllers to SalesController

--- ims_backend/src/routes/saleRoute.ts
+++ ims_backend/src/routes/saleRoute.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { protect } from '../middleware/authMiddleware';
 import { validateRequest } from '../middleware/validateRequest';
 import { createSaleSchema } from '../validation/sale/createSaleSchema';
-import { SalesController } from '../controllers/SalesControllers';
+import { SalesController } from '../controllers/SalesController';
 import { SalesServiceV1 } from '../services/SalesService';
 import { SalesRepository } from '../repositories/SalesRepository';
 import { CustomerRepository } from '../repositories/CustomerRepository';
