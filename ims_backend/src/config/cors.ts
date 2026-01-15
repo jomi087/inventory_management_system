3f7e114981147550d9887c5c443713151faed021
jomi087 <jomijoseph087@gmail.com>
2026-01-15 14:29:31 +0530

    cors error debug 1

--- ims_backend/src/config/cors.ts
+++ ims_backend/src/config/cors.ts
@@ -1,6 +1,6 @@
 
 export const corsOptions = {
-    origin: process.env.FRONTEND_URL,
+    origin:"https://inventory-management-system-five-sable.vercel.app",
     // credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: [
