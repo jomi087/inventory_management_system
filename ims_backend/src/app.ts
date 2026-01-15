e9eb3a32eb119fe272820e9e747704d41d00f7ef
jomi087 <jomijoseph087@gmail.com>
2026-01-15 16:25:48 +0530

    cors debug 4

--- ims_backend/src/app.ts
+++ ims_backend/src/app.ts
@@ -8,6 +8,10 @@ import { errorHandler } from './middleware/errorhandler';
 export const app = express();
 
 app.use(cors(corsOptions));
+app.use((req, res, next) => {
+    console.log('REQUEST ORIGIN =>', req.headers.origin);
+    next();
+});
 
 app.use(express.json({ limit: process.env.BODY_LIMIT }));
 app.use(
